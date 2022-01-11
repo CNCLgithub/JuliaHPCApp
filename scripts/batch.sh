4a91b2db06f28fa3a760f485ed9128d8fa469d0a
belledon <mbelledonne@gmail.com>
2022-01-11 15:06:51 -0500

    running on milgram

--- scripts/batch.sh
+++ scripts/batch.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 #SBATCH --job-name=JuliaHPCExample
-#SBATCH --out="slurm-%j.out"
+#SBATCH --out="slurm_out/%A_%a.out"
 #SBATCH --time=00:10:00
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=2GB
 #SBATCH --partition=scavenge
 #SBATCH --requeue
-#SBATCH --array=0-1199
+#SBATCH --array=0-200
 
-singularity exec cont.sif julia scripts/main.jl
+export JULIA_PROJECT="${PWD}"
+# singularity exec -c -B "${PWD}:${PWD}" cont.sif julia "${PWD}/scripts/main.jl"
+singularity exec -c -B "${PWD}:${PWD}" cont.sif julia -J "${PWD}/JHPC.so" \
+	                                                 "${PWD}/scripts/main.jl"
